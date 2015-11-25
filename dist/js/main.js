f3b4f3fa6230273f924f1b66e7bbe0490489b016
tien.tranhuu94 <tien.tranhuu94@gmail@gmail.com>
2015-11-26 00:31:01 +0700

    udpate modal login

--- dist/js/main.js
+++ dist/js/main.js
@@ -1,7 +1,24 @@
 var myApp = angular.module('myApp', ['mgcrea.ngStrap', 'ngRoute']);
 // header controller
-myApp.controller('HeaderCtrl', ['$rootScope', '$scope', function($rootScope, $scope) {
-    $scope.hello = 'hello world';
+myApp.controller('HeaderCtrl', ['$rootScope', '$scope', '$modal', function($rootScope, $scope, $modal) {
+    function showModal(template) {
+        // Pre-fetch an external template populated with a custom scope
+        var modal = $modal({
+            $scope: $scope,
+            template: template
+        });
+        // Hide modal
+        $rootScope.hideModal = function() {
+            modal.$promise.then(modal.hide);
+        };
+    }
+
+    function showModalLogin() {
+        showModal('/views/include/login.html');
+    }
+
+
+    $scope.showModalLogin = showModalLogin;
 }]);
 myApp.controller('LoginCtrl', ['$rootScope', '$scope', function($rootScope, $scope) {
 
@@ -9,8 +26,8 @@ myApp.controller('LoginCtrl', ['$rootScope', '$scope', function($rootScope, $sco
 
 // config angular
 myApp.config(function($routeProvider, $locationProvider) {
-    $routeProvider.otherwise('/')	
-    .when('/', {
-        templateUrl: '/views/index.html',
-    });
+    $routeProvider.otherwise('/')
+        .when('/', {
+            templateUrl: '/views/home/index.html',
+        });
 });
